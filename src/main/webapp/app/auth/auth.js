c88e5a022dd8b6acad60a3505adad295b29267c9
Christoffer Børrild <christoffer@boerrild.dk>
2014-08-20 11:22:24 +0200

    ECLOUD-73: Login failure is mow reflected on login page Also, introduced busy-indicator and refactored activation of login dialog

--- src/main/webapp/app/auth/auth.js
+++ src/main/webapp/app/auth/auth.js
@@ -22,230 +22,231 @@
 angular.module('mcp.auth', ['ui.bootstrap', 'http-auth-interceptor', 'ngStorage'])
 
 
-/* Constants */
+    /* Constants */
 
 // Authentication events broadcasted on rootScope
-.constant('AUTH_EVENTS', {
-  loginSuccess: 'auth-login-success',
-  loginFailed: 'auth-login-failed',
-  loginCancelled: 'auth-login-cancelled',
-  logoutSuccess: 'auth-logout-success',
-  sessionTimeout: 'auth-session-timeout',
-  notAuthenticated: 'auth-not-authenticated',
-  notAuthorized: 'auth-not-authorized'
-})
+    .constant('AUTH_EVENTS', {
+      loginSuccess: 'auth-login-success',
+      loginFailed: 'auth-login-failed',
+      loginCancelled: 'auth-login-cancelled',
+      logoutSuccess: 'auth-logout-success',
+      sessionTimeout: 'auth-session-timeout',
+      notAuthenticated: 'auth-not-authenticated',
+      notAuthorized: 'auth-not-authorized'
+    })
 
-.constant('USER_ROLES', {
-  all: '*',
-  admin: 'ADMIN',
-  user: 'USER',
-  editor: 'editor',
-  guest: 'guest'
-})
+    .constant('USER_ROLES', {
+      all: '*',
+      admin: 'ADMIN',
+      user: 'USER',
+      editor: 'editor',
+      guest: 'guest'
+    })
 
 
-/* Controllers */
+    /* Controllers */
 
 // A container for global application logic
-.controller('ApplicationController', function($rootScope, $scope, $modal, $location, $localStorage, USER_ROLES, AUTH_EVENTS, AuthService, httpAuthInterceptorService, Session) {
-  $scope.sidebar = { isMinified: false };
-  $scope.userRoles = USER_ROLES;
-  $scope.isAuthorized = AuthService.isAuthorized;
-  $scope.isLoggedIn = AuthService.isAuthenticated;
-  $scope.navigationTarget = null;
-  $scope.message = null;
-  $scope.alertMessages = [];
-  $scope.$storage = $localStorage.$default({
-    userSession: {
-      id: null,
-      userId: null,
-      userRole: null,
-      user: {name: null, role: null}
-    }
-  });
-  Session.importFrom($scope.$storage.userSession);
-  $scope.currentUser = Session.user;
-
-  $scope.$watch('$storage.userSession.userId', function() {
-    console.log("User session changed! ", $scope.$storage.userSession.userId);
-    $scope.currentUser = $scope.$storage.userSession.user;
-  }, true);
-
-  // Login listener that binds the login session to current user upon login success
-  $scope.$on(AUTH_EVENTS.loginSuccess, function() {
-    console.log("EVENT: User logged in! ", Session.user, "Session: ", Session);
-    $scope.currentUser = Session.user;
-    Session.exportTo($scope.$storage.userSession);
-    // Process pending requests
-    httpAuthInterceptorService.loginConfirmed();
-    // Navigate to defered page
-    if ($scope.navigationTarget) {
-      console.log("forwarding to defered path: ", $scope.navigationTarget);
-      $location.path($scope.navigationTarget).replace();
+    .controller('ApplicationController', function($rootScope, $scope, $modal, $location, $localStorage, USER_ROLES, AUTH_EVENTS, AuthService, httpAuthInterceptorService, Session) {
+      $scope.sidebar = {isMinified: false};
+      $scope.userRoles = USER_ROLES;
+      $scope.isAuthorized = AuthService.isAuthorized;
+      $scope.isLoggedIn = AuthService.isAuthenticated;
       $scope.navigationTarget = null;
-    }
-  });
-
-  // Logout listener that cleans up state bound to the current user
-  $scope.$on(AUTH_EVENTS.logoutSuccess, function() {
-    console.log("EVENT: User logged out! Session: ", Session);
-
-    //Clean up state
-    //delete $scope.$storage;
-    $scope.$storage.$reset({
-      userSession: {
-        id: null,
-        userId: null,
-        userRole: null,
-        user: null
-      }
-    });
-    Session.importFrom($scope.$storage.userSession);
-    $scope.currentUser = Session.user;
-
-    //$scope.currentUser = null;
-    $scope.navigationTarget = null;
-    $location.path('/').replace();
-    //Session.exportTo($scope.$storage.userSession);
-  });
-
-  // Login listener that resets current user upon login failure
-  $scope.$on(AUTH_EVENTS.loginFailed, function() {
-    console.log("User login failed!");
-    //$scope.currentUser = null;
-    //$scope.navigationTarget = null;
-    $scope.alertMessages = ["User login failed!"];
-    $scope.openLoginDialog();
-  });
-
-  // Login listener that resets any pending requests if user cancels login
-  $scope.$on(AUTH_EVENTS.loginCancelled, function() {
-    console.log("User login cancelled!");
-    httpAuthInterceptorService.loginCancelled();
-  });
-
-  // Login listener that warns that user is not authorized for the action
-  $scope.$on(AUTH_EVENTS.notAuthorized, function() {
-    console.log("User not authorized!");
-    $scope.alertMessages = ["User not authorized!"];
-    // TODO: flash an error message somehow...!?
-  });
-
-  // Login listener that brings up the login dialog whenver the "event:auth-loginRequired!" event is fired
-  $scope.$on('event:auth-loginRequired', function() {
-    console.log("event:auth-loginRequired!");
-    $scope.openLoginDialog();
-  });
-  
-  $scope.$on(AUTH_EVENTS.notAuthenticated, function(event, targetRoute) {
-    console.log("AUTH_EVENTS.notAuthenticated targetRoute: ", targetRoute," event:", event);
-    $scope.navigationTarget = targetRoute;
-    $scope.openLoginDialog('sm');
-  });
-
-  $scope.openLoginDialog = function(size) {
-    var modalInstance = $modal.open({
-      templateUrl: 'auth/loginDialog.html',
-      //controller: ModalInstanceController,
-      size: 'sm',
-      backdrop: 'static',
-      resolve: {
-//        user: function() {
-//          return $scope.user;
-//        }
-      }
-    });
-
-    modalInstance.result.then(function(credentials) {
-      // User pressed OK
-      console.log('Login dialog comitted: ', credentials, new Date());
-      AuthService.login(credentials).then(function() {
-        $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
-      }, function() {
-        $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+      $scope.message = null;
+      $scope.alertMessages = [];
+      $scope.$storage = $localStorage.$default({
+        userSession: {
+          id: null,
+          userId: null,
+          userRole: null,
+          user: {name: null, role: null}
+        }
       });
-    }, function() {
-      // User pressed CANCEL
-      console.log('Login cancelled at: ' + new Date());
-      $rootScope.$broadcast(AUTH_EVENTS.loginCancelled);
-    });
-  };
+      Session.importFrom($scope.$storage.userSession);
+      $scope.currentUser = Session.user;
 
-  $scope.logout = function() {
-    console.log("Logging out...");
-    AuthService.logout().then(function() {
-      $rootScope.$broadcast(AUTH_EVENTS.logoutSuccess);
-    }, function() {
-      $rootScope.$broadcast(AUTH_EVENTS.logoutFailed);
-    });
-  };
+      $scope.$watch('$storage.userSession.userId', function() {
+        console.log("User session changed! ", $scope.$storage.userSession.userId);
+        $scope.currentUser = $scope.$storage.userSession.user;
+      }, true);
 
-})
+      // Login listener that binds the login session to current user upon login success
+      $scope.$on(AUTH_EVENTS.loginSuccess, function() {
+        console.log("EVENT: User logged in! ", Session.user, "Session: ", Session);
+        $scope.currentUser = Session.user;
+        Session.exportTo($scope.$storage.userSession);
+        // Process pending requests
+        httpAuthInterceptorService.loginConfirmed();
+        // Navigate to defered page
+        if ($scope.navigationTarget) {
+          console.log("forwarding to defered path: ", $scope.navigationTarget);
+          $location.path($scope.navigationTarget).replace();
+          $scope.navigationTarget = null;
+        }
+      });
 
+      // Logout listener that cleans up state bound to the current user
+      $scope.$on(AUTH_EVENTS.logoutSuccess, function() {
+        console.log("EVENT: User logged out! Session: ", Session);
 
-/* Services */
-
-// Service logic related to the remote authentication and authorization (access control)
-.service('AuthService', function($http, Session) {
-  self = this;
-  this.login = function(credentials) {
-    console.log("Logging in with " + credentials.username);
-    return $http
-        .post('/rest/authentication/login', credentials, {ignoreAuthModule: true})
-        .then(function(respone) {
-          var data = respone.data;
-          console.log("Login response data: ", data);
-          Session.create(data.id, data.username, data.role);
+        //Clean up state
+        //delete $scope.$storage;
+        $scope.$storage.$reset({
+          userSession: {
+            id: null,
+            userId: null,
+            userRole: null,
+            user: null
+          }
         });
-  };
-  this.isAuthenticated = function() {
-    return !!Session.userId;
-  };
-  this.isAuthorized = function(authorizedRoles) {
-    if (!angular.isArray(authorizedRoles)) {
-      authorizedRoles = [authorizedRoles];
-    }
-    return (self.isAuthenticated() &&
-        authorizedRoles.indexOf(Session.userRole) !== -1);
-  };
-  this.logout = function() {
-    console.log("Logging out...");
-    return $http
-        .post('/rest/authentication/logout', {}, {ignoreAuthModule: true})
-        .then(function(respone) {
-          var data = respone.data;
-          console.log("Logout response data: ", data);
-          Session.destroy();
+        Session.importFrom($scope.$storage.userSession);
+        $scope.currentUser = Session.user;
+
+        //$scope.currentUser = null;
+        $scope.navigationTarget = null;
+        $location.path('/').replace();
+        //Session.exportTo($scope.$storage.userSession);
+      });
+
+      // Login listener that listens for login failure
+      $scope.$on(AUTH_EVENTS.loginFailed, function() {
+        console.log("User login failed!");
+      });
+
+      // Login listener that resets any pending requests if user cancels login
+      $scope.$on(AUTH_EVENTS.loginCancelled, function() {
+        console.log("User login cancelled!");
+        httpAuthInterceptorService.loginCancelled();
+      });
+
+      // Login listener that warns that user is not authorized for the action
+      // ( broadcasted by app.js in case of page-transition to a page that 
+      //   the user is not authorized to visit )
+      $scope.$on(AUTH_EVENTS.notAuthorized, function() {
+        console.log("User not authorized to visit this page!");
+        $scope.alertMessages = ["User not authorized!"];
+        // TODO: flash an error message somehow...!?
+      });
+
+      // Login listener that brings up the login dialog whenver the "event:auth-loginRequired!" event is fired
+      $scope.$on('event:auth-loginRequired', function() {
+        console.log("event:auth-loginRequired!");
+        $scope.openLoginDialog();
+      });
+
+      $scope.$on(AUTH_EVENTS.notAuthenticated, function(event, targetRoute) {
+        console.log("AUTH_EVENTS.notAuthenticated targetRoute: ", targetRoute, " event:", event);
+        $scope.navigationTarget = targetRoute;
+        $scope.openLoginDialog();
+      });
+
+      $scope.openLoginDialog = function() {
+        $modal.open({
+          templateUrl: 'auth/loginDialog.html',
+          controller: 'LoginController',
+          size: 'sm',
+          backdrop: 'static',
+        }).result.then(function() {
+          // Login dialog closed
+        }, function() {
+          // Login dialog dismissed (user pressed CANCEL or ESCAPE)
+          $rootScope.$broadcast(AUTH_EVENTS.loginCancelled);
         });
-  };
+      };
 
-})
+      $scope.logout = function() {
+        console.log("Logging out...");
+        AuthService.logout().then(function() {
+          $rootScope.$broadcast(AUTH_EVENTS.logoutSuccess);
+        }, function() {
+          $rootScope.$broadcast(AUTH_EVENTS.logoutFailed);
+        });
+      };
 
-// the user’s session information
-.service('Session', function() {
-  this.create = function(sessionId, userId, userRole) {
-    this.id = sessionId;
-    this.userId = userId;
-    this.userRole = userRole;
-    this.user = {name: userId, role: userRole};
-  };
-  this.importFrom = function(userSession) {
-    this.id = userSession.id;
-    this.userId = userSession.userId;
-    this.userRole = userSession.userRole;
-    this.user = userSession.user;
-  };
-  this.exportTo = function(userSession) {
-    userSession.id = this.id;
-    userSession.userId = this.userId;
-    userSession.userRole = this.userRole;
-    userSession.user = this.user;
-  };
-  this.destroy = function() {
-    this.id = null;
-    this.userId = null;
-    this.userRole = null;
-    this.user = null;
-  };
-  return this;
-});
+    })
+
+    .controller('LoginController', ['$scope', '$rootScope', 'AuthService', 'AUTH_EVENTS',
+      function($scope, $rootScope, AuthService, AUTH_EVENTS) {
+
+        $scope.login = function(credentials) {
+          $scope.loginPromise = AuthService.login(credentials).then(function() {
+            $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+            $scope.$close();
+          }, function() {
+            $scope.alert = "Incorrect username or password";
+            $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+          });
+        };
+
+      }])
+
+
+    /* Services */
+
+    // AuthService
+    // Service logic related to the remote authentication and authorization 
+    .service('AuthService', function($http, Session) {
+      self = this;
+      this.login = function(credentials) {
+        console.log("Logging in with " + credentials.username);
+        return $http
+            .post('/rest/authentication/login', credentials, {ignoreAuthModule: true})
+            .then(function(respone) {
+              var data = respone.data;
+              console.log("Login response data: ", data);
+              Session.create(data.id, data.username, data.role);
+            });
+      };
+      this.isAuthenticated = function() {
+        return !!Session.userId;
+      };
+      this.isAuthorized = function(authorizedRoles) {
+        if (!angular.isArray(authorizedRoles)) {
+          authorizedRoles = [authorizedRoles];
+        }
+        return (self.isAuthenticated() &&
+            authorizedRoles.indexOf(Session.userRole) !== -1);
+      };
+      this.logout = function() {
+        console.log("Logging out...");
+        return $http
+            .post('/rest/authentication/logout', {}, {ignoreAuthModule: true})
+            .then(function(respone) {
+              var data = respone.data;
+              console.log("Logout response data: ", data);
+              Session.destroy();
+            });
+      };
+
+    })
+
+    // Session
+    // the user’s session information
+    .service('Session', function() {
+      this.create = function(sessionId, userId, userRole) {
+        this.id = sessionId;
+        this.userId = userId;
+        this.userRole = userRole;
+        this.user = {name: userId, role: userRole};
+      };
+      this.importFrom = function(userSession) {
+        this.id = userSession.id;
+        this.userId = userSession.userId;
+        this.userRole = userSession.userRole;
+        this.user = userSession.user;
+      };
+      this.exportTo = function(userSession) {
+        userSession.id = this.id;
+        userSession.userId = this.userId;
+        userSession.userRole = this.userRole;
+        userSession.user = this.user;
+      };
+      this.destroy = function() {
+        this.id = null;
+        this.userId = null;
+        this.userRole = null;
+        this.user = null;
+      };
+      return this;
+    });
