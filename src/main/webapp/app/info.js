d340501283a034b027feab78bc15014b7ad00918
Christoffer Børrild <christoffer@boerrild.dk>
2014-10-29 16:02:16 +0100

    ECLOUD-105: added "env."-prefix to jenkins environment variables

--- src/main/webapp/app/info.js
+++ src/main/webapp/app/info.js
@@ -3,10 +3,10 @@ mcpInfo = {
   version: '${project.version}',
   isInitialised: '${project.version}'.lastIndexOf('project.version') === -1,
   build: { // filled in by jenkins build job
-    id: '${BUILD_ID}',
-    tag: '${BUILD_TAG}',
-    url: '${BUILD_URL}',
-    number: '${BUILD_NUMBER}',
-    isInitialised: '${BUILD_NUMBER}'.lastIndexOf('BUILD_NUMBER') === -1,
+    id: '${env.BUILD_ID}',
+    tag: '${env.BUILD_TAG}',
+    url: '${env.BUILD_URL}',
+    number: '${env.BUILD_NUMBER}',
+    isInitialised: '${env.BUILD_NUMBER}'.lastIndexOf('env.BUILD_NUMBER') === -1
   }
 };
